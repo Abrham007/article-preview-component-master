586d09eb9c7f85906450f795019c55efbad7914d
Abrham007 <abrhamaraya111@gamil.com>
2023-08-23 17:21:45 +0300

    add the link bubble

--- js/index.js
+++ js/index.js
@@ -1,11 +1,15 @@
 var shareButton = document.querySelector(".profile__div");
 var linkBubble = document.querySelector(".link-bubble");
+var shareArrow = document.querySelector("svg");
 shareButton.addEventListener("click", () => {
   if (linkBubble.style.display === "none") {
     linkBubble.style.display = "flex";
     shareButton.style.backgroundColor = "hsl(214, 17%, 51%)";
+    shareArrow.style.fill = "hsl(210, 46%, 95%)"
+
   } else {
     linkBubble.style.display = "none";
     shareButton.style.backgroundColor = "hsl(210, 46%, 95%)";
+    shareArrow.style.fill = "hsl(214, 17%, 51%)"
   }
 });
